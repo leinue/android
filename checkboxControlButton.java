93b759d2fbbda8b30d35f0940013e4f5a5f9891b
ivy <597055914@qq.com>
2014-12-18 22:59:30 +0800

    update

---
+++ checkboxControlButton.java
@@ -0,0 +1,54 @@
+package com.example.check_button_avaiable;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		CheckBox v=(CheckBox)findViewById(R.id.v);
+		final Button btn=(Button)findViewById(R.id.button1);
+		v.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+			
+			public void onCheckedChanged(CompoundButton buttonView,boolean isChecked){
+				Toast.makeText(MainActivity.this,"sassa",Toast.LENGTH_SHORT).show();
+				if(isChecked){
+					btn.setEnabled(true);
+				}else{
+					btn.setEnabled(false);
+				}
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
