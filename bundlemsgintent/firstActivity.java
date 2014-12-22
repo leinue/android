5d6ea4c25c0d5c5e7ee6dca53e6ff28db140fa78
ivy <597055914@qq.com>
2014-12-22 19:04:02 +0800

    update

---
+++ bundlemsgintent/firstActivity.java
@@ -0,0 +1,12 @@
+package com.example.bundlemsgintent;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class firstActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstActivity_layout);
+    }
+}
