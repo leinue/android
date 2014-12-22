2671c2d0376b7d408c39a72dff3e0f26fd97681c
ivy <597055914@qq.com>
2014-12-22 20:09:48 +0800

    update nittpmtpicj

--- buttontouch/MainActivity.java
+++ buttontouch/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        Toast.makeText(MainActivity.this,getResources().getString(R.string.app_name),Toast.LENGTH_LONG).show();
+        
         Button btn=(Button)findViewById(R.id.button1);
         btn.setOnClickListener(new OnClickListener(){
 
