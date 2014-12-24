179c50ccc3f1a392677d9afeac971e3f2d48eeb7
ivy <597055914@qq.com>
2014-12-24 21:46:00 +0800

    update function

---
+++ accountsoft/dao/InaccountDAO.java
@@ -0,0 +1,126 @@
+package com.example.accountsoft.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.accountsoft.model.Tb_inaccount;
+
+public class InaccountDAO {
+	
+	private DBOpenHelper helper;
+	private SQLiteDatabase db;
+	
+	public InaccountDAO(Context context){
+		helper=new DBOpenHelper(context);
+	}
+	
+	/**
+	 * 添加收入信息
+	 * 
+	 * @param tb_inaccount
+	 */
+	public void add(Tb_inaccount tb_inaccount){
+		db=helper.getWritableDatabase();
+		db.execSQL(
+				"insert into tb_inaccount (_id,money,time,type,handler,mark) values (?,?,?,?,?,?)",
+				new Object[] { tb_inaccount.getid(), tb_inaccount.getMoney(),
+						tb_inaccount.getTime(), tb_inaccount.getType(),
+						tb_inaccount.getHandler(), tb_inaccount.getMark() });
+	}
+	
+	/**
+	 * 更新收入信息
+	 * 
+	 * @param tb_inaccount
+	 */
+	public void update(Tb_inaccount tb_inaccount) {
+		db = helper.getWritableDatabase();// 初始化SQLiteDatabase对象
+		// 执行修改收入信息操作
+		db.execSQL(
+				"update tb_inaccount set money = ?,time = ?,type = ?,handler = ?,mark = ? where _id = ?",
+				new Object[] { tb_inaccount.getMoney(), tb_inaccount.getTime(),
+						tb_inaccount.getType(), tb_inaccount.getHandler(),
+						tb_inaccount.getMark(), tb_inaccount.getid() });
+	}
+	
+	/**
+	 * 查找收入信息
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Tb_inaccount find(int id){
+		db=helper.getWritableDatabase();
+		Cursor cursor = db
+				.rawQuery(
+						"select _id,money,time,type,handler,mark from tb_inaccount where _id = ?",
+						new String[] { String.valueOf(id) });// 根据编号查找收入信息，并存储到Cursor类中
+		if(cursor.moveToNext()){
+			return new Tb_inaccount(
+					cursor.getInt(cursor.getColumnIndex("_id")),
+					cursor.getDouble(cursor.getColumnIndex("money")),
+					cursor.getString(cursor.getColumnIndex("time")),
+					cursor.getString(cursor.getColumnIndex("type")),
+					cursor.getString(cursor.getColumnIndex("handler")),
+					cursor.getString(cursor.getColumnIndex("mark")));
+		}
+		return null;// 如果没有信息，则返回null
+	}
+	
+	/*
+	 *删除收入信息 
+	 * 
+	 * @param ids
+	 */
+	public void delete(Integer... ids){
+		if(ids.length>0){
+			StringBuffer sb=new StringBuffer();
+			for(int i=0;i<ids.length;i++){
+				sb.append('?').append(',');
+			}
+			sb.deleteCharAt(sb.length()-1);
+			db=helper.getWritableDatabase();
+			db.execSQL("delete from tb_inaccount where _id in("+sb+")", (Object[]) ids);
+		}
+	}
+	
+	/*
+	 * 获取收入信息
+	 * @param start 起始位置 
+	 * @param count 每页显示数量
+	 * @return
+	 */
+	public List<Tb_inaccount> getScrollData(int start,int count){
+		List<Tb_inaccount> tb_inaccount=new ArrayList<Tb_inaccount>();
+		db=helper.getWritableDatabase();
+		Cursor cursor = db.rawQuery("select * from tb_inaccount limit ?,?",
+				new String[] { String.valueOf(start), String.valueOf(count) });
+		while(cursor.moveToNext()){
+			tb_inaccount.add(new Tb_inaccount(cursor.getInt(cursor
+					.getColumnIndex("_id")), cursor.getDouble(cursor
+					.getColumnIndex("money")), cursor.getString(cursor
+					.getColumnIndex("time")), cursor.getString(cursor
+					.getColumnIndex("type")), cursor.getString(cursor
+					.getColumnIndex("handler")), cursor.getString(cursor
+					.getColumnIndex("mark"))));
+		}
+		return tb_inaccount;
+	}
+	
+	/*
+	 * 获取总记录数 
+	 * @return
+	 */
+	public long getCount(){
+		db=helper.getWritableDatabase();
+		Cursor cursor=db.rawQuery("select count(_id) from tb_inaccount", null);
+		if(cursor.moveToNext()){
+			return cursor.getLong(0);
+		}
+		return 0;
+	}
+}
