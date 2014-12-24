875b3c7deeed813795e9daf3a51b1a03b519ab45
ivy <597055914@qq.com>
2014-12-24 22:16:19 +0800

    update exit

--- accountsoft/MainActivity.java
+++ accountsoft/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.accountsoft;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +14,13 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button btnClose=(Button)findViewById(R.id.button2);
+		btnClose.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				finish();
+			}
+		});
 	}
 
 	@Override
